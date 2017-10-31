6e3abaf661917dfc8ce16afb1602f59a3896e77a
Filipe André de Matos Bicho <filipebch@gmail.com>
2017-10-31 16:59:52 +0100

    Small details to fix

--- Play.java
+++ Play.java
@@ -9,8 +9,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
-import java.util.Scanner;
-import java.io.*;
+
 
 public class Play {
 
@@ -27,7 +26,6 @@ public class Play {
 	private ArrayList<Cards> table = new ArrayList<>();
 	private ArrayList<Cards> hand1 = new ArrayList<>();
 	private ArrayList<Cards> hand2= new ArrayList<>();
-	private ArrayList<Cards> tempTable= new ArrayList<>();
 	
 	int[] result = new int[2];
 	float[] playerOdds = new float[3];
@@ -66,7 +64,7 @@ public class Play {
 			table.clear();
 			
 			System.out.println("\n ******************************** Game nº " + (i+1) + "*********************************** \n");
-			 
+			 i++;
 			// Shuffle the deck
 			deck = new Deck();
 			
@@ -77,7 +75,7 @@ public class Play {
 			cards.put(0, player1);
 			cards.put(1, player2);
 
-			showPlayersChips(pokerChips);
+			showComputers(pokerChips);
 			
 			// Pre Flop bets
 			pokerChips = bet.betsPreFlop(pokerChips, posDealer, playerOdds, 1, cards);
@@ -94,17 +92,18 @@ public class Play {
 			// Get flop cards
 			dealer.giveFlop(deck, table);
 			System.out.println("\n Flop: " + table);
+			cards.put(2, table);
 			
 			// If one of the players don't have chips the game is finish
 			if(pokerChips[0] == 0 || pokerChips[1] == 0)
 			{
-				tempTable.addAll(table);	
-				playerOdds = odds.oddsPlayerVSPlayerFlop(player1, player2, tempTable);
-				tempTable.clear();
+				playerOdds = odds.oddsPlayerVSPlayerFlop(player1, player2, table);
+				table.remove(4);
+				table.remove(3);
 				showPlayersOdds(pokerChips);
 			}
 			else
-				showPlayersChips(pokerChips);
+				showComputers(pokerChips);
 			
 			// If both players still have poker chips 
 			if(pokerChips[0] > 0 && pokerChips[1] > 1)
@@ -122,17 +121,18 @@ public class Play {
 			// Get turn
 			dealer.giveOneCard(deck, table);
 			System.out.println("\n Turn: " + table);
+			cards.put(2, table);
 			
 			// If one of the players don't have chips the game is finish
 			if(pokerChips[0] == 0 || pokerChips[1] == 0)
 			{
-				tempTable.addAll(table);	
-				playerOdds = odds.oddsPlayerVSPlayerTurn(player1, player2, tempTable);
-				tempTable.clear();
+				playerOdds = odds.oddsPlayerVSPlayerTurn(player1, player2, table);
+				table.remove(4);
+
 				showPlayersOdds(pokerChips);
 			}
 			else
-				showPlayersChips(pokerChips);
+				showComputers(pokerChips);
 			
 			// If both players still have poker chips 
 			if(pokerChips[0] > 0 && pokerChips[1] > 1)
@@ -150,6 +150,7 @@ public class Play {
 			// Get River
 			dealer.giveOneCard(deck, table);
 			System.out.println("\n River: " + table);
+			cards.put(2, table);
 			
 			// If both players still have poker chips 
 			if(pokerChips[0] > 0 && pokerChips[1] > 1)
@@ -164,7 +165,7 @@ public class Play {
 				continue;
 			}
 			
-			showPlayersChips(pokerChips);
+			showComputers(pokerChips);
 				
 			result[0] = evaluate.evaluateHand(player1, table);
 			hand1.addAll(evaluate.getHand());
@@ -213,12 +214,419 @@ public class Play {
 		}
 	}
 	
-	// Method to show both player poker chips
-	private void showPlayersChips(float[] pokerChips)
+	// Method that simulates a User vs User game
+	public void UserVsComputer(int gameMode)
+	{
+		// Select randomly a dealer
+		Random dealerRandom = new Random();
+		int posDealer = dealerRandom.nextInt((1-0)+1)+0;
+		
+		/* 0 - player 1 poker chips
+		 * 1 - player 2 poker chips
+		 * 2 - pot poker chips
+		 * 3 - if game continues or not
+		 */
+		float[] pokerChips = new float[4];
+		
+		// Both players start with 1500 chips
+		pokerChips[0] = 1500;
+		pokerChips[1] = 1500;
+		
+		// Game continue while game is true
+		Boolean game = true;
+		int i=0;
+		
+		// Game
+		while(game)
+		{
+			player1.clear();
+			player2.clear();
+			hand1.clear();
+			hand2.clear();
+			table.clear();
+			cards.clear();
+			
+			System.out.println("\n ******************************** Game nº " + (i+1) + "*********************************** \n");
+			 i++;
+			// Shuffle the deck
+			deck = new Deck();
+			
+			// Give the cards to the players
+			dealer.giveCards(deck, player1, player2);
+			
+			// Store players cards
+			cards.put(0, player1);
+			cards.put(1, player2);
+			
+			// Computer gets his winning odds before the flop
+			playerOdds[1] = (float) odds.preFlopOdds(player2);
+			
+			showPlayer(pokerChips);
+
+			// Pre Flop bets
+			pokerChips = bet.betsPreFlop(pokerChips, posDealer, playerOdds, gameMode, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get flop cards
+			dealer.giveFlop(deck, table);
+			System.out.println("-------------------------- Flop -----------------------------");
+			cards.put(2, table);
+			
+			// If one of the players don't have chips the game is finish
+			if(pokerChips[0] == 0 || pokerChips[1] == 0)
+			{
+				playerOdds = odds.oddsPlayerVSPlayerFlop(player1, player2, table);
+				table.remove(4);
+				table.remove(3);
+				showPlayersOdds(pokerChips);
+			}
+			else
+				showPlayer(pokerChips);
+			
+			// Computer gets his odds in flop
+			playerOdds = odds.oddsFlop(player2, table);
+			table.remove(4);
+			table.remove(3);
+			
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 1, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get turn
+			dealer.giveOneCard(deck, table);
+			System.out.println("-------------------------- Turn -----------------------------");
+			cards.put(2, table);
+			
+			// If one of the players don't have chips the game is finish
+			if(pokerChips[0] == 0 || pokerChips[1] == 0)
+			{
+				
+				playerOdds = odds.oddsPlayerVSPlayerTurn(player1, player2, table);
+				table.remove(4);
+				showPlayersOdds(pokerChips);
+			}
+			else
+				showPlayer(pokerChips);
+			
+			// Computer gets his odds in turn
+			playerOdds = odds.oddsTurn(player2, table);
+			table.remove(4);
+									
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 2, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get River
+			dealer.giveOneCard(deck, table);
+			System.out.println("-------------------------- River -----------------------------");
+
+			cards.put(2, table);
+			
+			// Computer gets his odds in flop
+			playerOdds = odds.oddsRiver(player2, table);
+			
+			showPlayer(pokerChips);
+			
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 3, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+				
+			result[0] = evaluate.evaluateHand(player1, table);
+			hand1.addAll(evaluate.getHand());
+			resultStr[0] = evaluate.getResult();
+			evaluate.reset();
+			
+			result[1] = evaluate.evaluateHand(player2, table);
+			hand2.addAll(evaluate.getHand());
+			resultStr[1] = evaluate.getResult();
+			evaluate.reset();
+			
+			if(Integer.valueOf(calcWinner.calculateWinner(hand1, hand2, result)).equals(1))
+			{
+				// Player 1 wins the pot
+				System.out.println("Player 1 wins");
+				pokerChips[0] += pokerChips[2];
+			}
+			else if (Integer.valueOf(calcWinner.calculateWinner(hand1, hand2, result)).equals(2))
+			{
+				// Player 2 wins the pot
+				System.out.println("Player 2 wins");
+				pokerChips[1] += pokerChips[2];
+			}
+			else
+			{
+				// If draw splits the pot
+				pokerChips[0] += pokerChips[2] / 2;
+				pokerChips[1] += pokerChips[2] / 2;
+				System.out.println("Draw");
+			}
+			
+			// Change dealer chip
+			posDealer = (posDealer == 0) ? 1 : 0;
+			pokerChips[2] = 0; //reset pot
+			if(pokerChips[0] <= 0)
+			{
+				System.out.println(" !!!!!!!!!!!!!!!! Player 2 won the game !!!!!!!!!!");
+				game = false;
+			}
+			else if(pokerChips[1] <= 0)
+			{
+				System.out.println(" !!!!!!!!!!!!!!!! Player 1 won the game !!!!!!!!!!");
+				game = false;
+			}				
+		}
+	}
+	
+	// Method that simulates a User vs User game
+	public void computerVsComputer(int gameMode)
+	{
+		// Select randomly a dealer
+		Random dealerRandom = new Random();
+		int posDealer = dealerRandom.nextInt((1-0)+1)+0;
+		
+		/* 0 - player 1 poker chips
+		 * 1 - player 2 poker chips
+		 * 2 - pot poker chips
+		 * 3 - if game continues or not
+		 */
+		float[] pokerChips = new float[4];
+		
+		// Both players start with 1500 chips
+		pokerChips[0] = 1500;
+		pokerChips[1] = 1500;
+		
+		// Game continue while game is true
+		Boolean game = true;
+		int i=0;
+		
+		// Game
+		while(game)
+		{
+			player1.clear();
+			player2.clear();
+			hand1.clear();
+			hand2.clear();
+			table.clear();
+			cards.clear();
+			
+			System.out.println("\n ******************************** Game nº " + (i+1) + "*********************************** \n");
+			 i++;
+			// Shuffle the deck
+			deck = new Deck();
+			
+			// Give the cards to the players
+			dealer.giveCards(deck, player1, player2);
+			
+			// Store players cards
+			cards.put(0, player1);
+			cards.put(1, player2);
+			
+			// Computers gets his winning odds before the flop
+			playerOdds[0] = (float) odds.preFlopOdds(player2);
+			playerOdds[1] = (float) odds.preFlopOdds(player2);
+			
+			showComputers(pokerChips);
+
+			// Pre Flop bets
+			pokerChips = bet.betsPreFlop(pokerChips, posDealer, playerOdds, gameMode, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get flop cards
+			dealer.giveFlop(deck, table);
+			System.out.println("-------------------------- Flop -----------------------------");
+			cards.put(2, table);
+			
+			// If one of the players don't have chips the game is finish
+			if(pokerChips[0] == 0 || pokerChips[1] == 0)
+			{
+				playerOdds = odds.oddsPlayerVSPlayerFlop(player1, player2, table);
+				table.remove(4);
+				table.remove(3);
+				showPlayersOdds(pokerChips);
+			}
+			else
+				showComputers(pokerChips);
+			
+			// Computer gets his odds in flop
+			playerOdds = odds.oddsFlop(player2, table);
+			table.remove(4);
+			table.remove(3);
+			
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 1, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get turn
+			dealer.giveOneCard(deck, table);
+			System.out.println("-------------------------- Turn -----------------------------");
+			cards.put(2, table);
+			
+			// If one of the players don't have chips the game is finish
+			if(pokerChips[0] == 0 || pokerChips[1] == 0)
+			{
+				
+				playerOdds = odds.oddsPlayerVSPlayerTurn(player1, player2, table);
+				table.remove(4);
+				showPlayersOdds(pokerChips);
+			}
+			else
+				showComputers(pokerChips);
+			
+			// Computer gets his odds in turn
+			playerOdds = odds.oddsTurn(player2, table);
+			table.remove(4);
+									
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 2, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+			
+			// Get River
+			dealer.giveOneCard(deck, table);
+			System.out.println("-------------------------- River -----------------------------");
+
+			cards.put(2, table);
+			
+			// Computer gets his odds in flop
+			playerOdds = odds.oddsRiver(player2, table);
+			
+			showComputers(pokerChips);
+			
+			// If both players still have poker chips 
+			if(pokerChips[0] > 0 && pokerChips[1] > 1)
+				pokerChips = bet.bets(pokerChips, posDealer, playerOdds, gameMode, 3, cards);
+			
+			// Check if the game continues
+			if(pokerChips[3] == 1)
+			{
+				// Change position if game stops
+				posDealer = (posDealer == 0) ? 1 : 0;
+				pokerChips[2] = 0; // pot reset
+				continue;
+			}
+				
+			result[0] = evaluate.evaluateHand(player1, table);
+			hand1.addAll(evaluate.getHand());
+			resultStr[0] = evaluate.getResult();
+			evaluate.reset();
+			
+			result[1] = evaluate.evaluateHand(player2, table);
+			hand2.addAll(evaluate.getHand());
+			resultStr[1] = evaluate.getResult();
+			evaluate.reset();
+			
+			if(Integer.valueOf(calcWinner.calculateWinner(hand1, hand2, result)).equals(1))
+			{
+				// Player 1 wins the pot
+				System.out.println("Player 1 wins");
+				pokerChips[0] += pokerChips[2];
+			}
+			else if (Integer.valueOf(calcWinner.calculateWinner(hand1, hand2, result)).equals(2))
+			{
+				// Player 2 wins the pot
+				System.out.println("Player 2 wins");
+				pokerChips[1] += pokerChips[2];
+			}
+			else
+			{
+				// If draw splits the pot
+				pokerChips[0] += pokerChips[2] / 2;
+				pokerChips[1] += pokerChips[2] / 2;
+				System.out.println("Draw");
+			}
+			
+			// Change dealer chip
+			posDealer = (posDealer == 0) ? 1 : 0;
+			pokerChips[2] = 0; //reset pot
+			if(pokerChips[0] <= 0)
+			{
+				System.out.println(" !!!!!!!!!!!!!!!! Player 2 won the game !!!!!!!!!!");
+				game = false;
+			}
+			else if(pokerChips[1] <= 0)
+			{
+				System.out.println(" !!!!!!!!!!!!!!!! Player 1 won the game !!!!!!!!!!");
+				game = false;
+			}				
+		}
+	}
+	
+	// Method to show player cards
+	private void showPlayer(float[] pokerChips)
 	{
 		System.out.println("---------------------------------------");
-		System.out.println(" ( " + pokerChips[0] + " )" + "Player 1: " + player1);
-		System.out.println(" ( " + pokerChips[1] + " )" + "Player 2: " + player2);
+		System.out.println("Player 1: " + player1 + " ( " + pokerChips[0] + " )");
+		System.out.println("Table: " + table);
+		System.out.println("Pot: " + pokerChips[2]);
+		System.out.println("---------------------------------------");
+	}
+	// Method to show both player poker chips
+	private void showComputers(float[] pokerChips)
+	{
+		System.out.println("---------------------------------------");
+		System.out.println("Player 1: " + player1 + " ( " + pokerChips[0] + " )");
+		System.out.println("Player 2: " + player2 + " ( " + pokerChips[1] + " )");
+		System.out.println("Table: " + table);
 		System.out.println("Pot: " + pokerChips[2]);
 		System.out.println("---------------------------------------");
 	}
@@ -227,8 +635,9 @@ public class Play {
 	private void showPlayersOdds(float[] pokerChips)
 	{
 		System.out.println("---------------------------------------");
-		System.out.println(" ( " + pokerChips[0] + " )" + "Player 1: " + player1 + " ( " + playerOdds[1] + " % )");
-		System.out.println(" ( " + pokerChips[1] + " )" + "Player 2: " + player2 + " ( " + playerOdds[2] + " % )");
+		System.out.println("Player 1: " + player1 + " ( " + playerOdds[1] + " % )" + " -  ( " + pokerChips[0] + " )");
+		System.out.println("Player 2: " + player2 + " ( " + playerOdds[2] + " % )" + " -  ( " + pokerChips[1] + " )");
+		System.out.println("Table: " + table);
 		System.out.println("Pot: " + pokerChips[2]);
 		System.out.println("---------------------------------------");
 	}
